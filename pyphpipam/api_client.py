b093c3a42e3af2f285397f25bba7f42cbb9d58cc
StevenRB <sbussell1066@gmail.com>
2018-11-20 09:29:24 -0500

    Updated logger

--- pyphpipam/api_client.py
+++ pyphpipam/api_client.py
@@ -6,161 +6,233 @@ import warnings
 
 # I use a local setup file in my pypath for configuration.
 #  This could be easily converted to use a json instead.
-from local_cfg import phpipamcfg
+# from srt_utilities import phpipamcfg
+
+logger = logging.getLogger()
+
+
+class phpipamcfg:
+
+    username = "apiuser"
+    password = "eQSAglYVr2"
+
+    url = "https://ipadmin.corpit.startek.com/api/testAppId/"
+    cert = False
+    ssl_warn = False
+
 
 # Disables the SSL warning alert
 if phpipamcfg.ssl_warn is False:
     warnings.filterwarnings("ignore")
 
-class IpAdmin(object):
 
+class IpAdmin(object):
     def __init__(self):
         self.base_url = phpipamcfg.url
         self.cert_path = phpipamcfg.cert
         self.__username = phpipamcfg.username
         self.__password = phpipamcfg.password
         self.__token = self.login()
-        self.headers = {'token': self.__token, 'Content-type': 'application/json'}
+        self.headers = {"token": self.__token, "Content-type": "application/json"}
 
     # The token that we're pulling here is an authorization code that gets put in the header of every request
     def login(self):
-        call = requests.post(self.base_url + 'user/', auth=(self.__username, self.__password), verify=self.cert_path)
+        call = requests.post(
+            self.base_url + "user/", auth=(self.__username, self.__password), verify=self.cert_path
+        )
         if call.status_code != 200:
             return "Login Error"
         else:
-            return json.loads(call.text)['data']['token']
+            return json.loads(call.text)["data"]["token"]
 
     # Retrieves a subnet's ID. Arg is a network, expressed as a CIDR string
     def get_subnet_id(self, subnet, simple=False):
-        call = requests.get(self.base_url + 'subnets/cidr/{}/'.format(subnet), headers=self.headers,
-                            verify=self.cert_path)
+        call = requests.get(
+            self.base_url + "subnets/cidr/{}/".format(subnet),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Picks the next subnet in the parent. Mask is bit length like with CIDR (int)
     def get_next_subnet(self, parent_id, mask, simple=False):
-        call = requests.get(self.base_url + 'subnets/{}/first_subnet/{}/'.format(parent_id, mask), headers=self.headers,
-                            verify=self.cert_path)
+        call = requests.get(
+            self.base_url + "subnets/{}/first_subnet/{}/".format(parent_id, mask),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Retrieves all addresses in a subnet. Returns a list of dictionaries
     def get_all_addresses(self, subnet_id, simple=False):
-        call = requests.get(self.base_url + 'subnets/{}/addresses/'.format(subnet_id), headers=self.headers,
-                            verify=self.cert_path)
+        call = requests.get(
+            self.base_url + "subnets/{}/addresses/".format(subnet_id),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Creates the next subnet in the parent. Mask is bit length like with CIDR (int)
     # Returns a dictionary with code, message, data (CIDR), id, success, and time
     def create_next_subnet(self, parent_id, mask, desc, simple=False):
         data = {"description": "{}".format(desc)}
-        call = requests.post(self.base_url + 'subnets/{}/first_subnet/{}/'.format(parent_id, mask), params=data,
-                             headers=self.headers, verify=self.cert_path)
+        call = requests.post(
+            self.base_url + "subnets/{}/first_subnet/{}/".format(parent_id, mask),
+            params=data,
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Retrieves an address's ID. Arg is a IP
     def get_address_id(self, ip_add, simple=False):
-        call = requests.get(self.base_url + 'addresses/search/{}/'.format(ip_add), headers=self.headers,
-                            verify=self.cert_path)
+        call = requests.get(
+            self.base_url + "addresses/search/{}/".format(ip_add),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Creates the next IP address in the parent subnet.
     def create_next_ip(self, parent_id, desc, simple=False):
         data = {"description": "{}".format(desc)}
-        call = requests.post(self.base_url + 'addresses/first_free/{}/'.format(parent_id), params=data,
-                             headers=self.headers, verify=self.cert_path)
+        call = requests.post(
+            self.base_url + "addresses/first_free/{}/".format(parent_id),
+            params=data,
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Creates specific subnet in the parent. Mask is bit length like with CIDR (int)
     def create_subnet(self, parent_id, network, mask, desc, simple=False):
-        data = {"masterSubnetId": "{}".format(parent_id),
-                "description": "{}".format(desc),
-                "subnet": "{}".format(network),
-                "mask": "{}".format(mask),
-                "sectionId": 5
-                }
-        call = requests.post(self.base_url + 'subnets/', params=data,
-                             headers=self.headers, verify=self.cert_path)
+        data = {
+            "masterSubnetId": "{}".format(parent_id),
+            "description": "{}".format(desc),
+            "subnet": "{}".format(network),
+            "mask": "{}".format(mask),
+            "sectionId": 5,
+        }
+        call = requests.post(
+            self.base_url + "subnets/", params=data, headers=self.headers, verify=self.cert_path
+        )
         return simple_return(call, simple)
 
     # Creates specific IP in the parent subnet
     def create_address(self, parent_id, ip, desc, simple=False):
-        data = {"subnetId": "{}".format(parent_id),
-                "description": "{}".format(desc),
-                "ip": "{}".format(ip),
-                "note": "Created via API"}
-        call = requests.post(self.base_url + 'addresses/', params=data, headers=self.headers, verify=self.cert_path)
+        data = {
+            "subnetId": "{}".format(parent_id),
+            "description": "{}".format(desc),
+            "ip": "{}".format(ip),
+            "note": "Created via API",
+        }
+        call = requests.post(
+            self.base_url + "addresses/", params=data, headers=self.headers, verify=self.cert_path
+        )
         return simple_return(call, simple)
 
     # Updates the description on a specific IP
     def update_address(self, address_id, desc, simple=False):
         data = {"description": "{}".format(desc)}
-        call = requests.patch(self.base_url + 'addresses/{}/'.format(address_id), params=data, headers=self.headers,
-                              verify=self.cert_path)
+        call = requests.patch(
+            self.base_url + "addresses/{}/".format(address_id),
+            params=data,
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Updates the description on a specific subnet
     def update_subnet(self, subnet_id, desc, simple=False):
         data = {"description": "{}".format(desc)}
-        call = requests.patch(self.base_url + 'subnets/{}/'.format(subnet_id), params=data, headers=self.headers,
-                              verify=self.cert_path)
+        call = requests.patch(
+            self.base_url + "subnets/{}/".format(subnet_id),
+            params=data,
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
         # Creates an L2 Domain using a name
+
     def create_domain(self, name, simple=False):
-        data = {"name": "{}".format(name)
-                }
-        call = requests.post(self.base_url + 'l2domains/', params=data,
-                             headers=self.headers, verify=self.cert_path)
+        data = {"name": "{}".format(name)}
+        call = requests.post(
+            self.base_url + "l2domains/", params=data, headers=self.headers, verify=self.cert_path
+        )
         return simple_return(call, simple)
 
     # Requests all L2Domains
     def getdomain(self):
-        call = requests.get(self.base_url + 'l2domains/', 
-                            headers=self.headers, verify=self.cert_path)
+        call = requests.get(
+            self.base_url + "l2domains/", headers=self.headers, verify=self.cert_path
+        )
         simple = False
         return simple_return(call, simple)
 
     # Creates a vlan. Default domain is #1
     def create_vlan(self, name, number, domain=1, simple=False):
-        data = {"name": "{}".format(name),
-                "domainId": "{}".format(domain),
-                "number": "{}".format(number)
-                }
-        call = requests.post(self.base_url + 'vlan/', params=data, headers=self.headers, verify=self.cert_path)
+        data = {
+            "name": "{}".format(name),
+            "domainId": "{}".format(domain),
+            "number": "{}".format(number),
+        }
+        call = requests.post(
+            self.base_url + "vlan/", params=data, headers=self.headers, verify=self.cert_path
+        )
         return simple_return(call, simple)
-
 
     # Creates location using a name and a street address (both string)
     def create_location(self, name, address, simple=False):
-        data = {"name": "{}".format(name),
-                "address": "{}".format(address)
-                }
-        call = requests.post(self.base_url + 'tools/locations/',
-                             headers=self.headers, params=data, verify=self.cert_path)
+        data = {"name": "{}".format(name), "address": "{}".format(address)}
+        call = requests.post(
+            self.base_url + "tools/locations/",
+            headers=self.headers,
+            params=data,
+            verify=self.cert_path,
+        )
         return simple_return(call, simple)
 
     # Deletes a subnet referenced by subnet ID. Will not delete child subnets
     # !!! DOES NOT ASK FOR VERIFICATION !!!
     def delete_subnet(self, subnet_id):
-        call = requests.delete(self.base_url + 'subnets/{}/'.format(subnet_id), headers=self.headers,
-                               verify=self.cert_path)
+        call = requests.delete(
+            self.base_url + "subnets/{}/".format(subnet_id),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         info = json.loads(call.text)
         return info
+
+    def get_address_info(self, address_id, simple=False):
+        call = requests.get(
+            self.base_url + "addresses/{}/".format(address_id),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
+        return simple_return(call, simple)
 
     # ############ DELETIONS ##############
 
     # Deletes all addresses in a subnet
     # !!! DOES NOT ASK FOR VERIFICATION !!!
     def delete_subnet_ips(self, subnet_id):
-        call = requests.delete(self.base_url + 'subnets/{}/truncate'.format(subnet_id), headers=self.headers,
-                               verify=self.cert_path)
+        call = requests.delete(
+            self.base_url + "subnets/{}/truncate".format(subnet_id),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         info = json.loads(call.text)
         return info
 
     # Delete a specific address
     # !!! DOES NOT ASK FOR VERIFICATION !!!
     def delete_address(self, address_id):
-        call = requests.delete(self.base_url + 'addresses/{}/'.format(address_id), headers=self.headers,
-                               verify=self.cert_path)
+        call = requests.delete(
+            self.base_url + "addresses/{}/".format(address_id),
+            headers=self.headers,
+            verify=self.cert_path,
+        )
         info = json.loads(call.text)
         return info
 
@@ -189,22 +261,40 @@ class IpAdmin(object):
         parent_id = self.get_subnet_id(parent_sub, simple)
         info = self.create_subnet(parent_id, network, mask, desc, simple)
         return info
-        
+
 
 class PhpIpAdminError(Exception):
     def __init__(self, *args):
-        error_str = ''
+        error_str = ""
         for arg in args:
-            error_str += ' | {}'.format(arg)
+            error_str += " | {}".format(arg)
         Exception.__init__(self, "PhpIpAdminError{}".format(error_str))
 
 
 def simple_return(call, simple):
     call_dict = json.loads(call.text)
-    if 'data' in call_dict:
+    if "data" in call_dict:
         return call_dict
     else:
         if simple is True:
             return False
         else:
-            return call_dict['message']
+            return call_dict["message"]
+
+
+if __name__ == "__main__":
+
+    print("Thanks for using Steve's awesome IP info tool")
+    ip = input("Please input your IP: ")
+
+    session = IpAdmin()
+    try:
+        ip_id = session.get_address_id(ip)
+    except Exception as e:
+        print("There was a general failure: {}".format(e))
+        exit()
+    if ip_id:
+        for k, v in ip_id["data"][0].items():
+            print("{}  -- {}".format(k, v))
+    else:
+        print("Sorry but no information is available")
